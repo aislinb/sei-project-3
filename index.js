c5301dae5fa7a7b95f3c2426cbb4c56ac1bc6797
Hugo Kinahan <hugokinahan@hotmail.com>
2021-01-08 11:34:11 +0000

    HK error handler created and added to index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import { port } from './config/environment.js'
 import connectToDatabase from './lib/connectToDb.js'
 import logger from './lib/logger.js'
+import errorHandler from './lib/errorHandler.js'
 
 const app = express()
 
@@ -16,6 +17,11 @@ async function startServer() {
 
     app.use(logger)
 
+    
+    
+    
+    app.use(errorHandler)
+    
     app.listen(port, () => console.log(`🤖 Up and running on port ${port}`))
   } catch (err) {
 
