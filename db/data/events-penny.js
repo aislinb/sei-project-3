ec4d6c6bf2a436f7540bb397057b69dc0e70a472
Penelope Jungreis <penelope.jungreis@gmail.com>
2021-01-08 01:42:43 +0000

    Wrote seed data objects for 2020 events in North America

--- db/data/events-penny.js
+++ db/data/events-penny.js
@@ -1,8 +1,152 @@
 export default [
   {
-    name: 'Beyonce', 
-    date: '2020-06-22', 
-    description: 'Beyonce was great!',
-    eventImage: 'https://i2-prod.getsurrey.co.uk/incoming/article11559662.ece/ALTERNATES/s1200/iam_JLA.jpg'
+    name: 'Ultra Music Festival', 
+    date: '2020-03-20', 
+    description: 'Ultra Music Festival 2020 kicked off the electronic music calendar year and this was sure a big one. Set in the heart of Miami, this three-day festival transported attendees to a musical dimension with each stage. The Ultra Music Festival 2020 headliners included Flume, Major Lazer, Afrojack, Zedd and Gesaffelstein.',
+    eventImage: 'https://ultramusicfestival.com/wp-content/uploads/2019/04/miami-gallery-2019-9-1600x900.jpg',
+    // Miami, FL
+  },
+  {
+    name: 'SXSW Music Festival', 
+    date: '2020-03-16', 
+    description: 'Added into the mix the incredible array of screenings, performances, seminars and presentations from leading figures in the worlds of film, culture, music and technology and you had an event that left audiences inspired, exhilarated and at the cutting edge of what was happening everywhere.',
+    eventImage: 'https://www.mediaplaynews.com/wp-content/uploads/2020/03/SXSW-1.jpg',
+    // Austin, TX
+  },
+  {
+    name: 'Coachella', 
+    date: '2020-04-10', 
+    description: 'Festival lineup highlights included Rage Against the Machine, Frank Ocean, Travis Scott, Calvin Harris, Flume, Lana Del Rey, Thom Yorke, Lil Uzi Vert, Run the Jewels, FKA Twigs, Disclosure, Fatboy Slim, Lil Nas X, slowthai, Megan Thee Stallion, and BigBang.',
+    eventImage: 'https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fblogs-images.forbes.com%2Fyolarobert1%2Ffiles%2F2019%2F04%2FFB-1200x628.jpg',
+    // Indio, CA
+  },
+  {
+    name: 'New Orleans Jazz & Heritage Festival ', 
+    date: '2020-04-23', 
+    description: 'The New Orleans Jazz & Heritage Festival was an event that rivaled even Mardi Gras in the Big Easy — and it was easy to see why! Launched in 1970, it brought together some of the largest talents in blues, R&B, gospel, Cajun, zydeco, Afro-Caribbean, folk, Latin, rock, rap, country, bluegrass and beyond for two weekends across the end of April and beginning of May.',
+    eventImage: 'https://midcitymessenger.com/wp-content/uploads/2019/01/8T8A2859.jpg',
+    // New Orleans
+  },
+  {
+    name: 'Bonnaroo', 
+    date: '2020-06-11', 
+    description: 'The 2020 lineup included 2019’s breakout star Lizzo as well as other megastars like Miley Cyrus, Bassnectar, Grand Ole Opry, Flume, Oysterhead, Lizzo, King Gizzard and the Lizard Wizard, Tame Impala, Tenacious D, Run the Jewels, Tool, Vampire Weekend and more.',
+    eventImage: 'https://static.billboard.com/files/media/bonnaroo-music-festival-generic-atmosphere-2018-billboard-1548-compressed.jpg',
+  },
+  {
+    name: 'Lollapalooza', 
+    date: '2020-07-30', 
+    description: 'Grace Potter was sickkk. From Ellie Gouding to Imagine Dragons to LL Cool J - Lollapalooza 2020 had something for everyone. The crowds loved it all.',
+    eventImage: 'https://weraveyou.com/wp-content/uploads/2020/05/lolla-argentina-photo-credit-alive-coverage.jpg',
+    // Chicago, IL
+  },
+  {
+    name: 'Burning Man', 
+    date: '2020-08-25', 
+    description: 'Burning Man is certainly not your average music festival. What it is remains open for debate, but it was described as a temporary metropolis dedicated to art and community. With incredible art installations, Rio Carnivale-worthy costumes and endless partying in the Nevada desert, Burning Man largely defied explanation and we are so glad we got to experience it.',
+    eventImage: 'https://s.hdnux.com/photos/01/05/40/04/18219291/3/1600x0.jpg',
+  },
+  {
+    name: 'My Chemical Romance', 
+    date: '2020-06-16', 
+    description: 'This concert was incredible. So many years since we last saw MCR on tour, it was great to have them back on the stage where they belong, eh?',
+    eventImage: 'https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/1304/2019/11/08044001/tmp748d4528-3af1-4bf8-a248-5ebd200f1e39.jpg',
+    // Toronto, ON
+  },
+  {
+    name: 'The Weeknd', 
+    date: '2020-06-12', 
+    description: 'The Weeknd wowed the Canadian crowds in his home country with his dark style of R&B and the mystique surrounding his identity. His falsetto and eccentric music style made this a night to remember.',
+    eventImage: 'https://www.japanconcerttickets.com/dev/wp-content/uploads/2018/08/The-Weeknd-2018.jpg',
+    // Vancouver
+  },
+  {
+    name: 'Alanis Morissette', 
+    date: '2020-05-09', 
+    description: '',
+    eventImage: '',
+  },
+  {
+    name: 'Taylor Swift', 
+    date: '2020-08-01', 
+    description: 'Taylor Swift made the Pats proud by country rocking their home statium in Foxborough.',
+    eventImage: 'https://werenotsorry.files.wordpress.com/2013/07/taylor-swift-red-world-tour-performance-in-glendale-17.jpg',
+    // Foxborough, MA
+  },
+  {
+    name: 'MTV Video Music Awards', 
+    date: '2020-08-30', 
+    description: 'J. Balvin brought a latin reggae flair to this year\'s ever-dramatic MTV Music Awards.',
+    eventImage: 'https://consequenceofsound.net/wp-content/uploads/2019/05/jbalvin-Arcoiris-Tour-dates-tickets-north-america.png?w=807',
+    // Brooklyn, NY
+  },
+  {
+    name: 'Comic Con', 
+    date: '2020-08-21', 
+    description: 'Comic Con was so much fun this year and the costumes were fantastic. The Gotham City Sirens especially brought it.',
+    eventImage: 'https://scontent-lhr8-2.xx.fbcdn.net/v/t1.0-9/1546146_10152328705112173_1294615083_n.jpg?_nc_cat=104&ccb=2&_nc_sid=cdbe9c&_nc_ohc=l7GqbuT58uIAX95DI6w&_nc_ht=scontent-lhr8-2.xx&oh=7c6c5fcf6cbfa9f6d37a90c944769968&oe=601E1923',
+    // Seattle, WA
+  },
+  {
+    name: 'Governers Ball', 
+    date: '2020-06-05', 
+    description: 'Many great days on Randalls Island this year in the Big Apple. Loved the food trucks this time.',
+    eventImage: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/ecb5dd68582105.5b61e637b6015.jpg',
+    // New York, NY
+  },
+  {
+    name: 'LA Gay Pride', 
+    date: '2020-09-01', 
+    description: 'Such a beautiful day in LA.',
+    eventImage: 'https://static.billboard.com/files/2020/03/la-pride-2019-billboard-1548-1584127332-768x433.jpg',
+  },
+  {
+    name: 'TriBeCa Film Festival', 
+    date: '2020-04-15', 
+    description: 'As always, got to see the most boundary-pushing new films presented at this festival.',
+    eventImage: 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fabinlot%2Ffiles%2F2019%2F08%2FTribecaOutdoorscreening-e1567224527250.jpg',
+    // New York, NY
+  },
+  {
+    name: 'Rockettes Christmas Spectacular', 
+    date: '2020-12-25', 
+    description: 'The kickline was fire.',
+    eventImage: 'https://media.timeout.com/images/100177945/630/472/image.jpg',
+    // New York, NY
+  },
+  {
+    name: 'Cirque de Soleil', 
+    date: '2020-03-15', 
+    description: 'Lions, tigers, and elephants - oh my! What a show it was this year. The acrobats and tight rope walkers kept the audience at the edge of their seats!',
+    eventImage: 'https://d15v4l58k2n80w.cloudfront.net/file/1396975600/54167053667/width=1280/height=720/format=-1/fit=crop/crop=327x331+2327x1309/rev=2/t=431914/e=never/k=8bc3ba40/CirqueDuSoleil_Luzia_Canes.jpg',
+    // Las Vegas, NV
+  },
+  {
+    name: 'Red Sox vs Yankees', 
+    date: '2020-06-13', 
+    description: 'The Red Sox totally trashed the Yankees. Gary Sanchez spotted crying on his exit from Fenway Park.',
+    eventImage: 'https://specials-images.forbesimg.com/imageserve/1152819482/960x0.jpg?fit=scale',
+    // Boston, MA
+  },
+  {
+    name: 'Boston Marathon', 
+    date: '2020-04-20', 
+    description: 'British Josh Domb finished in first place with a record-breaking time. Heart break hill almost crushed him but he pulled ahead like a champ.',
+    eventImage: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/boylston2-boston19-1587155893.jpg',
+    // Boston, MA
+  },
+  {
+    name: 'Lakers vs Heat', 
+    date: '2020-10-07', 
+    description: 'LeBron James totally Lebroned it',
+    eventImage: 'https://cdn.newsapi.com.au/image/v1/bdf9ce38c8874272f8afcbb8326fe190',
+    // Los Angeles, CA
+  },
+  {
+    name: 'Ironman Campeche', 
+    date: '2020-03-15', 
+    description: 'They swam. They ran. They cycled. They pushed it to the limit.',
+    eventImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv4OfvfJVgg52xrtuOJHqblskpR1IlvwEueQ&usqp=CAU',
+    // Campeche, Mexico
   }
 ]
